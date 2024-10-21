e06b23145d7703196c66a36c4ce7ae0eb340db3d
lierniSR <lierniSR@github.com>
2024-10-21 20:12:27 +0200

    Visualizacion de ventana modal para modificar una persona creada

---
+++ src/main/java/es/liernisarraoa/gestiondepersonasmodal2/Controladores/ModificarPersonaController.java
@@ -0,0 +1,22 @@
+package es.liernisarraoa.gestiondepersonasmodal2.Controladores;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class ModificarPersonaController {
+    @FXML
+    private TextField nombreTextField;
+
+    @FXML
+    private TextField apellidoTextField;
+
+    @FXML
+    private TextField edadTextField;
+
+    public void guardarPersona(ActionEvent actionEvent) {
+    }
+
+    public void cerrarModal(ActionEvent actionEvent) {
+    }
+}
